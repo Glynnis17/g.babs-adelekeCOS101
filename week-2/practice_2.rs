f42e7919af364c0bfe7065891183daf66e33a5b4
Glynnis17 <glynnis.babs-adeleke@pau.edu.ng>
2026-08-31 23:45:51 +0100

    Class Assignment

---
+++ week-2/practice_2.rs
@@ -0,0 +1,7 @@
+fn main() {
+	println!();
+	println!("hello");
+	println!("format {} arguments", "some");
+	println!("My name is {}. I am a member of COS101 stream 1","Babs-Adeleke Glynnis" );
+	
+}
