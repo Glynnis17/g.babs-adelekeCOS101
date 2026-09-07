78b2d6acd9b02d0131f06d8196e226b058c55e4b
Glynnis17 <glynnis.babs-adeleke@pau.edu.ng>
2026-09-07 17:19:13 +0100

    Delete week-3/practice_1 directory

--- week-3/practice_1/target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Glynnis\OneDrive - Pan-Atlantic University\g.babs-adelekeCOS101\week-3\practice_1\target\debug\deps\practice_1.d: src\main.rs
-
-C:\Users\Glynnis\OneDrive - Pan-Atlantic University\g.babs-adelekeCOS101\week-3\practice_1\target\debug\deps\practice_1.exe: src\main.rs
-
-src\main.rs:
--- week-3/practice_1/target/debug/practice_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Glynnis\OneDrive\ -\ Pan-Atlantic\ University\g.babs-adelekeCOS101\week-3\practice_1\target\debug\practice_1.exe: C:\Users\Glynnis\OneDrive\ -\ Pan-Atlantic\ University\g.babs-adelekeCOS101\week-3\practice_1\src\main.rs
