3f5e52a71f8a6b9be67d7144d46f9a37e132bda3
Glynnis17 <glynnis.babs-adeleke@pau.edu.ng>
2026-09-07 19:37:30 +0100

    Practice_10

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let sum = 5500 + 7310;
+    println!("The sum of 5500 and 7310 is {}",sum);
+
+    let difference:f32 = 95.5 - 4.3;
+    println!("The difference between 95.5 and 4.3 is {}",difference);
+
+    let product:u32 = 4 * 30;
+    println!("The product of 4 and 30 is {}",product);
+
+    let quotient = 56.7 / 32.2;
+    println!("The divison of 56.7 and 32.2 is {}",quotient);
+
+    let remainder = 43 % 5;
+    println!("The remainder of 43 and 5 is {}",remainder);
+}
