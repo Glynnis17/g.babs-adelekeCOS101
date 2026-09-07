e3dbb26972ff23f94f1de2c97e4c28533649f24b
Glynnis17 <glynnis.babs-adeleke@pau.edu.ng>
2026-09-07 17:51:41 +0100

    Practice_1

---
+++ week-3/practice_1/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let school_string = "School of Science and Technology";
+    let rating_float = 5.0;
+    let is_growing_boolean = true;
+    let department_integer = 3;
+
+    println!("School Name is: {}",school_string);
+    println!("The school rating on a scale of 1 to 5 is: {}",rating_float);
+    println!("The school is growing: {}",is_growing_boolean);
+    println!("The number of departments is: {}",department_integer);
+}
