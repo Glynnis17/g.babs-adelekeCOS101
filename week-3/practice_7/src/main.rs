8faa24f19cf5d05ba59406ee639299b3f3058336
Glynnis17 <glynnis.babs-adeleke@pau.edu.ng>
2026-09-07 19:13:07 +0100

    Practice_7

---
+++ week-3/practice_7/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let float_with_separator = 11_000.555_001;
+    println!("Float Value is {}",float_with_separator);
+
+    let integer_with_separator = 50_000;
+    println!("Integer Value is {}",integer_with_separator);
+}
---
+++ week-3/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\Glynnis\OneDrive\ -\ Pan-Atlantic\ University\g.babs-adelekeCOS101\week-3\practice_7\target\debug\practice_7.exe: C:\Users\Glynnis\OneDrive\ -\ Pan-Atlantic\ University\g.babs-adelekeCOS101\week-3\practice_7\src\main.rs
